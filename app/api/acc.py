8dded21dfc020f7fe30582454a10c4d9443925c2
Simon Persson <simon@akep.se>
2018-04-14 12:21:31 +0200

    Removed firebase

--- app/api/acc.py
+++ app/api/acc.py
@@ -1,23 +1,7 @@
 from app import app
-from firebase_admin import firestore
 from app.tsn.accidents import r
 
-db = firestore.client()
-
-@app.route('/acc')
+@app.route('/api/acc')
 def publish():
-    collection = db.collection('poi')
     result = r.json()
-    for item in result['value']:
-        collection.document(str(item['Id'])).set({
-            u'lng': item['Longitud'],
-            u'lat': item['Latitud']
-        })
-    return "hello"
-
-@app.route('/read')
-def read():
-    docs = db.collection('poi').get()
-    for doc in docs:
-        print(u'{} => {}'.format(doc.id, doc.to_dict()))
-    return "t"
+    return str(result['value'])
