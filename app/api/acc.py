3cae3e106bb1828185c31d6852c1d51acaa3b48d
Simon Persson <simon@akep.se>
2018-04-14 14:18:05 +0200

    Prepare for heroku

--- app/api/acc.py
+++ app/api/acc.py
@@ -1,7 +1,13 @@
 from app import app
 from app.tsn.accidents import r
+from app.common import geojson
+import json
 
 @app.route('/api/acc')
 def publish():
-    result = r.json()
-    return str(result['value'])
+    new_result = list()
+    result = r.json()['value']
+    for item in result:
+        new_result.append(geojson.convert(item))
+    result = json.dumps(new_result, ensure_ascii=False)
+    return result.encode('utf8')
---
+++ app/common/geojson.py
@@ -0,0 +1,21 @@
+
+
+def convert(obj):
+    geojson = {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [obj['Latitud'], obj['Longitud']]
+        },
+        "properties": {
+            "name": "Dinagat Islands",
+            "location_type": obj['Platstyp'],
+            "accident_type": obj['Olyckstyp'],
+            "intensity": obj['Svarighetsgrad'],
+            "road_condition": obj['Vaglag'],
+            "weather": obj['Vaderlek'],
+            "light_condition": obj['Ljusforhallande']
+        }
+    }
+
+    return geojson
