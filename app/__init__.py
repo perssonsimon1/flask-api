ebfb171de6afbad43a8a8410a812fd00e1a0b353
Simon Persson <simon@akep.se>
2018-04-14 14:41:51 +0200

    Removed cors header

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,6 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-app.config['CORS_HEADERS'] = 'Content-Type'
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
